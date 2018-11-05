27c6a94212871136266b01b25be971305c8c1ac3
Hudognig <kopev.job@gmail.com>
2018-11-06 01:20:57 +0300

    Fixed bug with google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  {
+    access_type: 'offline',
+    scope: 'userinfo.email, userinfo.profile, plus.me',
+    redirect_uri: 'https://cracozyabra.com/auth/google_oauth2/callback',
+    prompt: 'select_account consent',
+    provider_ignores_state: true
+  }
 end
