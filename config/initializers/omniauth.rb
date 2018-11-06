0f700f3e13d3fa339b82ea70e4a8b435dd9323dc
Hudognig <kopev.job@gmail.com>
2018-11-06 03:42:21 +0300

    Finally fixed google oauth2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
   {
     access_type: 'offline',
     scope: 'userinfo.email, userinfo.profile, plus.me',
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  get 'willow', to: 'willow#index', as: 'index'
   get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'willow', to: 'willow#index'
-  get 'me', to: 'me#show', as: 'me'
+  get 'auth/failure', to: 'willow#index'
 
   root 'willow#index'
 end
