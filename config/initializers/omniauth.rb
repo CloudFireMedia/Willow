6fb35e87333b8c5783af5b7d3b2526846d6f349d
Hudognig <kopev.job@gmail.com>
2018-11-06 17:45:02 +0300

    Expanded omniauth scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
   {
     access_type: 'offline',
-    scope: 'userinfo.email, userinfo.profile, plus.me',
+    scope: 'userinfo.email, userinfo.profile, plus.me, drive, drive.appdata, drive.file, drive.metadata, drive.metadata.readonly, drive.photos.readonly, drive.readonly, drive.scripts',
     redirect_uri: 'https://cracozyabra.com/auth/google_oauth2/callback',
     prompt: 'select_account consent',
     provider_ignores_state: true
